f865c9a4ae46f64342a6a0a4af787abcce5c5553
PhungNgocTan123 <phungngoctandev@gmail.com>
2022-08-27 08:16:32 +0700

    add validation with Joi

--- src/library/logging.ts
+++ src/library/logging.ts
@@ -2,7 +2,7 @@ import chalk from "chalk";
 
 export default class Logging {
     public static log = (args: any) => this.info(args);
-    public static info = (args: any) => console.log(chalk.blue(`[${new Date().toLocaleString()}] [INFO]`), typeof args === 'string' ? chalk.blueBright(args) : args);
-    public static warn = (args: any) => console.log(chalk.yellow(`[${new Date().toLocaleString()}] [INFO]`), typeof args === 'string' ? chalk.yellowBright(args) : args);
-    public static error = (args: any) => console.log(chalk.red(`[${new Date().toLocaleString()}] [INFO]`), typeof args === 'string' ? chalk.redBright(args) : args);
+    public static info = (args: any, namespace?: string) => console.log(chalk.blue(`[${new Date().toLocaleString()}] [INFO] [${namespace}]`), typeof args === 'string' ? chalk.blueBright(args) : args);
+    public static warn = (args: any, namespace?: string) => console.log(chalk.yellow(`[${new Date().toLocaleString()}] [WARN] [${namespace}]`), typeof args === 'string' ? chalk.yellowBright(args) : args);
+    public static error = (args: any, namespace?: string) => console.log(chalk.red(`[${new Date().toLocaleString()}] [ERROR] [${namespace}]`), typeof args === 'string' ? chalk.redBright(args) : args);
 }
--- src/routes/Author.ts
+++ src/routes/Author.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import controller from '../controller/Author';
+import { ValidateJoi, Shcemas } from '../middleware/ValidateSchema';
 
 const router = express.Router();
 
 // Create author
-router.post('/create', controller.creatAuthor);
+router.post('/create', ValidateJoi(Shcemas.author.create), controller.creatAuthor);
 
 // Get author
 router.get('/get/:authorId', controller.readAuthor);
@@ -13,8 +14,9 @@ router.get('/get/:authorId', controller.readAuthor);
 router.get('/get/', controller.readAllAuthor);
 
 // Update author
-router.patch('/update/:authorId', controller.updateAuthor);
+router.patch('/update/:authorId', ValidateJoi(Shcemas.author.update), controller.updateAuthor);
 
 // Delete author
 router.delete('/delete/:authorId', controller.deleteAuthor);
 
+export = router;
